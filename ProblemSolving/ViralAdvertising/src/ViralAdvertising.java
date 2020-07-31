5aaa2328c3e654a1a6488ef9c050628d51be7e6c
Alex <o.a.momotyuk@gmail.com>
2020-07-30 21:32:34 -0500

    the ViralAdvertising exercise initial commit

---
+++ ProblemSolving/ViralAdvertising/src/ViralAdvertising.java
@@ -0,0 +1,35 @@
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ViralAdvertising {
+
+    // Complete the viralAdvertising function below.
+    static int viralAdvertising(int n) {
+
+        return 0;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
