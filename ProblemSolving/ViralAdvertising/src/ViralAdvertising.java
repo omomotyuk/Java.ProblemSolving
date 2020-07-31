c3c60a876a22513e09ab57ac1d8f67027581f57f
Alex <o.a.momotyuk@gmail.com>
2020-07-30 21:49:55 -0500

    solved the ViralAdvertising exercise

--- ProblemSolving/ViralAdvertising/src/ViralAdvertising.java
+++ ProblemSolving/ViralAdvertising/src/ViralAdvertising.java
@@ -12,23 +12,35 @@ public class ViralAdvertising {
     // Complete the viralAdvertising function below.
     static int viralAdvertising(int n) {
 
-        return 0;
+        int shared = 5;
+        int cumulative = 0;
+
+        int i = 1;
+        while (i < n + 1) {
+            cumulative += shared / 2;
+            shared = shared / 2 * 3;
+            i++;
+        }
+
+        return cumulative;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         int result = viralAdvertising(n);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
+        System.out.println(result);
 
-        bufferedWriter.close();
+        //bufferedWriter.write(String.valueOf(result));
+        //bufferedWriter.newLine();
+
+        //bufferedWriter.close();
 
         scanner.close();
     }
