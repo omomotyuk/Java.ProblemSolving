c0d59fce97d0e8beeb594c0c837c14a675f066a3
Alex <o.a.momotyuk@gmail.com>
2020-08-17 22:09:27 -0500

    solved the CaesarCipher exercise

--- ProblemSolving/CaesarCipher/src/CaesarCipher.java
+++ ProblemSolving/CaesarCipher/src/CaesarCipher.java
@@ -11,13 +11,31 @@ public class CaesarCipher {
     // Complete the caesarCipher function below.
     static String caesarCipher(String s, int k) {
 
-        return "";
+        String output = "";
+
+        k = k % 26;
+
+        char c;
+        int i = 0;
+        while (i < s.length()) {
+            c = s.charAt(i);
+            if ( 'a' <= c && c <= 'z') {
+                output += (char) ('a' + (c - 'a' + k) % 26);
+            } else if ('A' <= c && c <= 'Z') {
+                output += (char) ('A' + (c - 'A' + k) % 26);
+            } else {
+                output += c;
+            }
+            i++;
+        }
+
+        return output;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
@@ -29,10 +47,12 @@ public class CaesarCipher {
 
         String result = caesarCipher(s, k);
 
-        bufferedWriter.write(result);
+        System.out.println(result);
+
+        /*bufferedWriter.write(result);
         bufferedWriter.newLine();
 
-        bufferedWriter.close();
+        bufferedWriter.close();*/
 
         scanner.close();
     }
