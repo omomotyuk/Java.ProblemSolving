0620fe9a84152de3d892de95a66d42f0a62d38bb
Alex <o.a.momotyuk@gmail.com>
2020-08-23 16:44:08 -0500

    solved the FunnyString exercise

--- ProblemSolving/FunnyString/src/FunnyString.java
+++ ProblemSolving/FunnyString/src/FunnyString.java
@@ -12,13 +12,32 @@ public class FunnyString {
     // Complete the funnyString function below.
     static String funnyString(String s) {
 
-        return "";
+        int i = 0;
+        int j = s.length() - 1;
+        int l = s.length() / 2 + 1;
+        int next;
+        int revs;
+
+        while (i < l) {
+            next = (int) (s.charAt(i + 1) - s.charAt(i++));
+            revs = (int) (s.charAt(j - 1) - s.charAt(j--));
+
+            if ( Math.abs(next) != Math.abs(revs) ) {
+                break;
+            }
+        }
+
+        if (i == l) {
+            return "Funny";
+        } else {
+            return "Not Funny";
+        }
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int q = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
@@ -28,11 +47,13 @@ public class FunnyString {
 
             String result = funnyString(s);
 
-            bufferedWriter.write(result);
-            bufferedWriter.newLine();
+            System.out.println(result);
+
+            //bufferedWriter.write(result);
+            //bufferedWriter.newLine();
         }
 
-        bufferedWriter.close();
+        //bufferedWriter.close();
 
         scanner.close();
     }
