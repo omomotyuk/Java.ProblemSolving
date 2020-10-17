fa44bbe64eb0495868de827a9d1bd16aecf64b84
Alex <o.a.momotyuk@gmail.com>
2020-10-17 12:10:20 -0500

    the TwoCharacters exercise first solution

--- ProblemSolving/TwoCharacters/src/TwoCharacters.java
+++ ProblemSolving/TwoCharacters/src/TwoCharacters.java
@@ -2,6 +2,7 @@
 import java.io.*;
 import java.math.*;
 import java.security.*;
+import java.sql.Array;
 import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
@@ -17,24 +18,51 @@ public class TwoCharacters {
     // Complete the alternate function below.
     static int alternate(String s) {
 
+        int [] numbers = new int[26];
 
-        return 0;
+        int i = 0;
+        while(i < s.length()) {
+
+            char c = s.charAt(i++);
+
+            if(c <= 'Z') {
+                numbers[c - 'A']++;
+            } else {
+                numbers[c - 'a']++;
+            }
+        }
+
+        Arrays.sort(numbers);
+
+        int result = 0;
+
+        i = 2;
+        while(i > 0) {
+            if(numbers[26 - i] != 0) {
+                result += numbers[26 - i--];
+            } else {
+                return 0;
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int l = Integer.parseInt(bufferedReader.readLine().trim());
 
         String s = bufferedReader.readLine();
 
         int result = alternate(s);
+        System.out.println(result);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
+        //bufferedWriter.write(String.valueOf(result));
+        //bufferedWriter.newLine();
 
         bufferedReader.close();
-        bufferedWriter.close();
+        //bufferedWriter.close();
     }
 }
