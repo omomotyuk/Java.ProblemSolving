a64504f32663311a6ba27436aa6a257b0bfd39a3
Alex <o.a.momotyuk@gmail.com>
2020-11-04 20:16:58 -0600

    the solution of the CountingSort1 exercise

--- ProblemSolving/CountingSort1/src/CountingSort1.java
+++ ProblemSolving/CountingSort1/src/CountingSort1.java
@@ -12,13 +12,20 @@ public class CountingSort1 {
     // Complete the countingSort function below.
     static int[] countingSort(int[] arr) {
 
-        return arr;
+        int[] result = new int[100];
+
+        int i = 0;
+        while (i < arr.length) {
+            result[arr[i++]]++;
+        }
+
+        return result;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
@@ -36,16 +43,20 @@ public class CountingSort1 {
         int[] result = countingSort(arr);
 
         for (int i = 0; i < result.length; i++) {
-            bufferedWriter.write(String.valueOf(result[i]));
+            //bufferedWriter.write(String.valueOf(result[i]));
+            System.out.print(result[i]);
 
             if (i != result.length - 1) {
-                bufferedWriter.write(" ");
+                //bufferedWriter.write(" ");
+                System.out.print(" ");
+            } else {
+                System.out.println();
             }
         }
 
-        bufferedWriter.newLine();
+        //bufferedWriter.newLine();
 
-        bufferedWriter.close();
+        //bufferedWriter.close();
 
         scanner.close();
     }
