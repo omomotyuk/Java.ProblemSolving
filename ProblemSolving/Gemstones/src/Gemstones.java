3ce8a26ce9fd1ba6feda79ca6408f4994c0367de
Alex <o.a.momotyuk@gmail.com>
2020-11-10 18:08:30 -0600

    the solution of the Gemstones exercise

--- ProblemSolving/Gemstones/src/Gemstones.java
+++ ProblemSolving/Gemstones/src/Gemstones.java
@@ -11,14 +11,37 @@ public class Gemstones {
 
     // Complete the gemstones function below.
     static int gemstones(String[] arr) {
+        
+        int[] gemstone = new int[26];
 
-        return 0;
+        int i;
+        for ( String stone : arr ) {
+            i = 0;
+            int[] check = new int[26];
+            while (i < stone.length()) {
+                int index = (int)(stone.charAt(i++) - 'a');
+                if(check[index] == 0) {
+                    gemstone[index]++;
+                }
+                check[index]++;
+            }
+        }
+
+        int result = 0;
+        i = 0;
+        while(i < gemstone.length) {
+            if(gemstone[i++] == arr.length) {
+                result++;
+            }
+        }
+
+        return result;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
@@ -32,10 +55,12 @@ public class Gemstones {
 
         int result = gemstones(arr);
 
-        bufferedWriter.write(String.valueOf(result));
+        /*bufferedWriter.write(String.valueOf(result));
         bufferedWriter.newLine();
 
-        bufferedWriter.close();
+        bufferedWriter.close();*/
+
+        System.out.println(result);
 
         scanner.close();
     }
