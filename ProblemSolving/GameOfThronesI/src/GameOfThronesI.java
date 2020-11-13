da1f8d3b154752de22ede4f7bacbf3a7ca115263
Alex <o.a.momotyuk@gmail.com>
2020-11-12 22:01:11 -0600

    the solution of the GameOfThronesI exercise

--- ProblemSolving/GameOfThronesI/src/GameOfThronesI.java
+++ ProblemSolving/GameOfThronesI/src/GameOfThronesI.java
@@ -12,22 +12,42 @@ public class GameOfThronesI {
     // Complete the gameOfThrones function below.
     static String gameOfThrones(String s) {
 
-        return s;
+        int[] pairs = new int[26];
+
+        int i = 0;
+        while (i < s.length()) {
+            pairs[s.charAt(i++) - 'a']++;
+        }
+
+        int odd = 0;
+
+        i = 0;
+        while (i < 26) {
+            odd += pairs[i++] % 2;
+        }
+
+        if (s.length() % 2 != odd) {
+            return "NO";
+        }
+
+        return "YES";
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         String s = scanner.nextLine();
 
         String result = gameOfThrones(s);
 
-        bufferedWriter.write(result);
+        System.out.println(result);
+
+        /*bufferedWriter.write(result);
         bufferedWriter.newLine();
 
-        bufferedWriter.close();
+        bufferedWriter.close();*/
 
         scanner.close();
     }
